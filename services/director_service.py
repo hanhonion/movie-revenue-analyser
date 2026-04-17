8685078089064d366dd45b854fe7d6ca1f9adb42
Pahm Anh <lethienmin@gmail.com>
2026-04-17 23:14:34 +0700

    Update director_service.py

--- services/director_service.py
+++ services/director_service.py
@@ -2,12 +2,12 @@ from models.director import Director
 from repositories.director_repo import DirectorRepository
 from repositories.movie_repo import MovieRepository
 from datetime import datetime
-
+ 
 class DirectorService:
     def __init__(self):
         self.repo = DirectorRepository()
         self.movie_repo = MovieRepository()
-
+ 
     def validate_director(self, director: Director):
         # Quy tắc 5.1: Tên đạo diễn không được trống
         if not director.name or not director.name.strip():
@@ -18,14 +18,18 @@ class DirectorService:
             current_year = datetime.now().year
             if director.birth_year > current_year:
                 raise ValueError(f"Năm sinh đạo diễn ({director.birth_year}) không thể lớn hơn năm hiện tại ({current_year}).")
-
+ 
     def add_director(self, director: Director):
         self.validate_director(director)
         return self.repo.add(director)
-
+ 
+    def update_director(self, director: Director):
+        self.validate_director(director)
+        return self.repo.update(director)
+ 
     def get_all_directors(self, search_query=None):
         return self.repo.get_all(search_query=search_query)
-
+ 
     def delete_director(self, director_id: int):
         # Quy tắc 4: Ràng buộc tham chiếu (Không xóa đạo diễn nếu còn phim)
         # Đây là quy tắc quan trọng trong quản lý DB
